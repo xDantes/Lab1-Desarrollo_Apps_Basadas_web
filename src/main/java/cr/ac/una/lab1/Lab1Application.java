30ced68e22fe3dde583ea3ff51109dd5743d40ba
derekurena956 <derekurena956@gmail.com>
2026-08-06 18:58:10 -0600

    test push from sourcetree

--- src/main/java/cr/ac/una/lab1/Lab1Application.java
+++ src/main/java/cr/ac/una/lab1/Lab1Application.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab1Application {
-
+public class Lab1Application { //test push
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1Application.class, args);
 	}
